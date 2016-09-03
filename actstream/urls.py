2ac4376133068940f63e0a629e9a8c5e738487b7
Akin Adebowale <akin@baseofficial.com>
2016-09-03 06:40:37 -0400

    Update urls.py

--- actstream/urls.py
+++ actstream/urls.py
@@ -1,11 +1,19 @@
 import django
+"""
 try:
     from django.conf.urls import url, patterns
 except ImportError:
     from django.conf.urls.defaults import url, patterns
 
 from actstream import feeds, views
+"""
 
+try:
+    from django.conf.urls import url
+except ImportError:
+    from django.conf.urls.defaults import url
+
+from actstream import feeds, views
 
 urlpatterns = [
     # User feeds
@@ -56,5 +64,7 @@ urlpatterns = [
     url(r'^$', views.stream, name='actstream'),
 ]
 
+"""
 if django.VERSION[:2] < (1, 9):
     urlpatterns = patterns('', *urlpatterns)
+"""
